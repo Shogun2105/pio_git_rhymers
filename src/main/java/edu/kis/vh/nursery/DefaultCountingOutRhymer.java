edceb076afc8e041ab7e229c989c52b53cb2f1e5
Shogun2105 <wc21410@wp.pl>
2021-01-19 19:03:37 +0100

    Zmieniono modyfikatory dostępu w plikach DefaultCountingOutRhymer i FIFORhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int INT = 12;
-    public static final int INT1 = -1;
+    private final int INT = 12;
+    private final int INT1 = -1;
     private int[] numbers = new int[INT];
 
-    public int total = INT1;
+    private int total = INT1;
 
     public void countIn(int in) {
         if (!isFull())
